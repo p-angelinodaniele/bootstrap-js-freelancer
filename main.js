0a2a5d43d4e0ba6bf4273d02330367a19dd81549
P.angelino <p.angelinodaniele@gmail.com>
2022-03-29 17:36:45 +0200

    colore rosso dopo immissione di sconto sbagliato

--- main.js
+++ main.js
@@ -13,17 +13,17 @@ const element = document.getElementById("bottone");
 element.addEventListener("click", importo)
 
 function importo(){
-    let moltiplicazione;
+    let totale;
     let typeOfWork = document.getElementById("tipoDiLavoro").value;
     let ore = parseFloat(document.getElementById("oreDiLavoro").value);
     if(typeOfWork == 1){
-        moltiplicazione = ore * 20.5;
+        totale = ore * 20.5;
     }else if(typeOfWork == 2){
-        moltiplicazione = ore * 15.3;
+        totale = ore * 15.3;
     }else if(typeOfWork == 3){
-        moltiplicazione = ore * 33.6;
+        totale = ore * 33.6;
     }
-    console.log(moltiplicazione);
+    
 
     let arrSconto =["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24",];
     const sconto=0.25;
@@ -31,17 +31,17 @@ function importo(){
     
     for(let i=0; i<=arrSconto.length; i++){
         if(discountCode == arrSconto[i]){
-            let risultatoSconto = moltiplicazione * sconto;
-            moltiplicazione = moltiplicazione - risultatoSconto;
-            
-            document.getElementById("totale").innerHTML = "Il prezzo finale è di " + moltiplicazione.toFixed(2) + "&euro;";
+            let risultatoSconto = totale * sconto;
+            totale = totale - risultatoSconto;
+            document.getElementById("totale").innerHTML = "Il prezzo finale è di " + totale.toFixed(2) + "&euro;";
             i = arrSconto.length;
 
         }else if(discountCode != arrSconto[i] && i==arrSconto.length ){
-            alert("Il codice inserito non è valido");
-            document.getElementById("totale").innerHTML = "Il prezzo finale è di " + moltiplicazione.toFixed(2) + "&euro;";
+            document.getElementById("discountCode").style.color="red";
+            document.getElementById("discountCode").style.borderColor="red";
+            document.getElementById("totale").innerHTML = "Il prezzo finale è di " + totale.toFixed(2) + "&euro;";
         }
-    } console.log(moltiplicazione);
+    } 
     
     
 
